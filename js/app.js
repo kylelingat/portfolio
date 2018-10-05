c03f8f3332e997856a1a0152e957c9bfaf04b440
kylelingat <kylejlingat@gmail.com>
2018-10-05 09:09:03 -1000

    obj

--- js/app.js
+++ js/app.js
@@ -1,33 +1,73 @@
 $("#arrowDown").click(function() {
     $('html,body').animate({
-        scrollTop: $("#t").offset().top},
+            scrollTop: $("#t").offset().top
+        },
         'slow');
 });
 
+var hackSeaInfo = {
+    img: "url(../images/hacksea.png)",
+    description: "For this project"
+}
+
+var haccInfo = {
+    img: "url(../images/hacc.png)",
+    description: "For this projectss"
+}
+
+var bluePlanetInfo = {
+    img: "url(../images/blueplanet.png)",
+    description: "For this projectssssssss444ssssss"
+}
+
+var marvelApiInfo = {
+    img: "url(../images/marvelApi.png)",
+    description: "For this projectssss3333sss"
+}
+
+var sushiInfo = {
+    img: "url(../images/kunio.png)",
+    description: "For this projectss12123sss"
+}
+var constructionInfo = {
+    img: "url(../images/construction.png)",
+    description: "For this projectzxczxcssss"
+}
 
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-
 $(".projectGridItem").click(function() {
-  modal.style.display = "block";
-  var projName = $(this).find(".projectTitle").text()
-  $("#projName").text(projName)
+    modal.style.display = "block";
+    var projName = $(this).find(".projectTitle").text()
+    $("#projName").text(projName);
+    if ($(this).attr("id") == "hackSeaContainer"){
+        $("#projImg").css("background-image", hackSeaInfo.img)
+        $("#projDesc").text(hackSeaInfo.description)
+    } else if ($(this).attr("id") == "hacc4EduContainer"){
+      $("#projImg").css("background-image", haccInfo.img)
+      $("#projDesc").text(haccInfo.description)
+    } else if ($(this).attr("id") == "bluePlanetContainer"){
+      $("#projImg").css("background-image", bluePlanetInfo.img)
+      $("#projDesc").text(bluePlanetInfo.description)
+    } else if ($(this).attr("id") == "marvelApiContainer"){
+      $("#projImg").css("background-image", marvelApiInfo.img)
+      $("#projDesc").text(marvelApiInfo.description)
+    } else if ($(this).attr("id") == "sushiWebContainer"){
+      $("#projImg").css("background-image", sushiInfo.img)
+      $("#projDesc").text(sushiInfo.description)
+    } else if ($(this).attr("id") == "constructionWebContainer"){
+      $("#projImg").css("background-image", constructionInfo.img)
+      $("#projDesc").text(constructionInfo.description)
+    }
 });
 
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
