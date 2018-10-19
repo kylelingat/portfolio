0b5c71d23d9df80c7861ea9e8bbfb9228789fa6a
kylelingat <kylejlingat@gmail.com>
2018-10-19 09:35:32 -1000

    test

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ $("#arrowDown").click(function() {
 
 var hackSeaInfo = {
     img: "url(./images/hacksea.png)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut quam nec velit molestie cursus a in lacus. Vestibulum pharetra eros augue, id tempus mauris aliquam ac. Maecenas in felis in augue gravida facilisis ac id purus. Cras sit amet ex semper, tempor orci nec, pulvinar sem. Pellentesque nunc diam.",
+    description: "This project was a hackathon project that placed second overall. The challenge was to design a website that improves debris reporting efficiency. I worked with a team of 5 (including me) and I took the role of the website designer and front-end developer. The main page follows a simple rounded box layout that links to the other pages. Every page has a vertical navigation menu.",
+    img2: "url(./images/hacksea2.png)",
+    description2: "This report debris page serves as a form for users to submit debris. This page improves reporting efficiency because it has better design and lets the user know what information to submit. The layout was made via CSS grid. The form doesn't actually 'submit' because we couldn't build a back-end in under 24 hours so it just submits mock data. ",
     site: "https://kylelingat.github.io/hack-for-the-sea/",
     gitHub: "https://github.com/kylelingat/hack-for-the-sea"
 }
@@ -56,32 +58,34 @@ $(".projectGridItem").click(function() {
     var projName = $(this).find(".projectTitle").text()
     $("#projName").text(projName);
     if ($(this).attr("id") == "hackSeaContainer"){
-        $("#projImgContainer").css("background-image", hackSeaInfo.img);
+        $("#projImg").css("background-image", hackSeaInfo.img);
         $("#projDesc").text(hackSeaInfo.description);
+        $("#projImg2").css("background-image", hackSeaInfo.img2);
+        $("#description2").text(hackSeaInfo.description2);
         $("#visitSiteButton > a").attr("href", hackSeaInfo.site);
         $("#visitGithubButton > a").attr("href", hackSeaInfo.gitHub);
     } else if ($(this).attr("id") == "hacc4EduContainer"){
-      $("#projImgContainer").css("background-image", haccInfo.img);
+      $("#projImg").css("background-image", haccInfo.img);
       $("#projDesc").text(haccInfo.description);
       $("#visitSiteButton > a").attr("href", haccInfo.site);
       $("#visitGithubButton > a").attr("href", haccInfo.gitHub);
     } else if ($(this).attr("id") == "bluePlanetContainer"){
-      $("#projImgContainer").css("background-image", bluePlanetInfo.img);
+      $("#projImg").css("background-image", bluePlanetInfo.img);
       $("#projDesc").text(bluePlanetInfo.description);
       $("#visitSiteButton > a").attr("href", bluePlanetInfo.site);
       $("#visitGithubButton > a").attr("href", bluePlanetInfo.gitHub);
     } else if ($(this).attr("id") == "marvelApiContainer"){
-      $("#projImgContainer").css("background-image", marvelApiInfo.img);
+      $("#projImg").css("background-image", marvelApiInfo.img);
       $("#projDesc").text(marvelApiInfo.description);
       $("#visitSiteButton > a").attr("href", marvelApiInfo.site);
       $("#visitGithubButton > a").attr("href", marvelApiInfo.gitHub);
     } else if ($(this).attr("id") == "sushiWebContainer"){
-      $("#projImgContainer").css("background-image", sushiInfo.img);
+      $("#projImg").css("background-image", sushiInfo.img);
       $("#projDesc").text(sushiInfo.description);
       $("#visitSiteButton > a").attr("href", sushiInfo.site);
       $("#visitGithubButton > a").attr("href", sushiInfo.gitHub);
     } else if ($(this).attr("id") == "constructionWebContainer"){
-      $("#projImgContainer").css("background-image", constructionInfo.img);
+      $("#projImg").css("background-image", constructionInfo.img);
       $("#projDesc").text(constructionInfo.description);
       $("#visitSiteButton > a").attr("href", constructionInfo.site);
       $("#visitGithubButton > a").attr("href", constructionInfo.gitHub);
