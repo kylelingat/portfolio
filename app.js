df3f11e430daa89d5831bdde3491e60348c9f15e
kylelingat <kylejlingat@gmail.com>
2018-11-09 09:13:12 -1000

    m

--- app.js
+++ app.js
@@ -51,60 +51,6 @@ var constructionInfo = {
 }
 
 function generateHTML(project){
-  var imgContainer = document.createElement("div");
-  imgContainer.id = project + "ImgContainer";
-
-  var projImg = document.createElement("div");
-  projImg.id = project + "Img1";
-
-  imgContainer.appendChild(projImg)
-
-  var visitSiteButton = document.createElement("div");
-  visitSiteButton.id = "visitSiteButton";
-  var redirectSite = document.createElement("a");
-  visitSiteButton.appendChild(redirectSite);
-  $(redirectSite).replaceWith('<a target="_blank" id="ta" href="">Visit Site   <i class="fas fa-directions"></i></a>')
-
-  var visitGithubButton = document.createElement("div");
-  visitGithubButton.id = "visitGithubButton";
-  var redirectGithub = document.createElement("a");
-  visitGithubButton.appendChild(redirectGithub)
-  $(redirectGithub).replaceWith('<a target="_blank" href="">Visit GitHub   <i class="fab fa-github"></i></i></a>')
-
-  var projDesc = document.createElement("p");
-  projDesc.id = project + "Desc1"
-
-  var imgContainer2 = document.createElement("div");
-  imgContainer2.id = project + "ImgContainer2";
-
-  var projImg2 = document.createElement("div");
-  projImg2.id = project + "Img2";
-
-  imgContainer2.appendChild(projImg2)
-
-  var projDesc2 = document.createElement("p");
-  projDesc2.id = project + "Desc2";
-
-  var imgContainer3 = document.createElement("div");
-  imgContainer3.id = project + "ImgContainer3";
-
-  var projImg3 = document.createElement("div");
-  projImg3.id = project + "Img3";
-
-  imgContainer3.appendChild(projImg3);
-
-  var projDesc3 = document.createElement("p");
-  projDesc3.id = project + "Desc3";
-
-  document.getElementById("modalContentGrid").appendChild(imgContainer)
-  document.getElementById("modalContentGrid").appendChild(visitSiteButton)
-  document.getElementById("modalContentGrid").appendChild(visitGithubButton)
-  document.getElementById("modalContentGrid").appendChild(projDesc)
-  document.getElementById("modalContentGrid").appendChild(imgContainer2)
-  document.getElementById("modalContentGrid").appendChild(projDesc2)
-  document.getElementById("modalContentGrid").appendChild(imgContainer3)
-  document.getElementById("modalContentGrid").appendChild(projDesc3)
-
 }
 
 
@@ -115,52 +61,52 @@ var span = document.getElementsByClassName("close")[0];
 $(".projectGridItem").click(function() {
     modal.style.display = "block";
     $('body').css("overflow", "hidden");
-    var projName = $(this).find(".projectTitle").text()
-    $("#projName").text(projName);
-    if ($(this).attr("id") == "hackSeaContainer"){
-        $("#modalContentGrid").empty();
-        generateHTML("hackSea")
-        $("#hackSeaImg1").css("background-image", hackSeaInfo.img);
-        $("#visitSiteButton > a").attr("href", hackSeaInfo.site);
-        $("#visitGithubButton > a").attr("href", hackSeaInfo.gitHub);
-        $("#hackSeaDesc1").text(hackSeaInfo.description);
-        $("#hackSeaImg2").css("background-image", hackSeaInfo.img2);
-        $("#hackSeaDesc2").text(hackSeaInfo.description2);
-        $("#hackSeaImg3").css("background-image", hackSeaInfo.img3);
-        $("#hackSeaDesc3").text(hackSeaInfo.description3);
-
-    } else if ($(this).attr("id") == "hacc4EduContainer"){
-      $("#modalContentGrid").empty();
-      generateHTML("hacc")
-      // $("#projImg").css("background-image", haccInfo.img);
-      // $("#projDesc").text(haccInfo.description);
-      // $("#visitSiteButton > a").attr("href", haccInfo.site);
-      // $("#visitGithubButton > a").attr("href", haccInfo.gitHub);
-    } else if ($(this).attr("id") == "bluePlanetContainer"){
-      $("#modalContentGrid").empty();
-      // $("#projImg").css("background-image", bluePlanetInfo.img);
-      // $("#projDesc").text(bluePlanetInfo.description);
-      // $("#visitSiteButton > a").attr("href", bluePlanetInfo.site);
-      // $("#visitGithubButton > a").attr("href", bluePlanetInfo.gitHub);
-    } else if ($(this).attr("id") == "marvelApiContainer"){
-      $("#modalContentGrid").empty();
-      // $("#projImg").css("background-image", marvelApiInfo.img);
-      // $("#projDesc").text(marvelApiInfo.description);
-      // $("#visitSiteButton > a").attr("href", marvelApiInfo.site);
-      // $("#visitGithubButton > a").attr("href", marvelApiInfo.gitHub);
-    } else if ($(this).attr("id") == "sushiWebContainer"){
-      $("#modalContentGrid").empty();
-      // $("#projImg").css("background-image", sushiInfo.img);
-      // $("#projDesc").text(sushiInfo.description);
-      // $("#visitSiteButton > a").attr("href", sushiInfo.site);
-      // $("#visitGithubButton > a").attr("href", sushiInfo.gitHub);
-    } else if ($(this).attr("id") == "constructionWebContainer"){
-      $("#modalContentGrid").empty();
-      // $("#projImg").css("background-image", constructionInfo.img);
-      // $("#projDesc").text(constructionInfo.description);
-      // $("#visitSiteButton > a").attr("href", constructionInfo.site);
-      // $("#visitGithubButton > a").attr("href", constructionInfo.gitHub);
-    }
+    // var projName = $(this).find(".projectTitle").text()
+    // $("#projName").text(projName);
+    // if ($(this).attr("id") == "hackSeaContainer"){
+    //     $("#modalContentGrid").empty();
+    //     generateHTML("hackSea")
+    //     $("#hackSeaImg1").css("background-image", hackSeaInfo.img);
+    //     $("#visitSiteButton > a").attr("href", hackSeaInfo.site);
+    //     $("#visitGithubButton > a").attr("href", hackSeaInfo.gitHub);
+    //     $("#hackSeaDesc1").text(hackSeaInfo.description);
+    //     $("#hackSeaImg2").css("background-image", hackSeaInfo.img2);
+    //     $("#hackSeaDesc2").text(hackSeaInfo.description2);
+    //     $("#hackSeaImg3").css("background-image", hackSeaInfo.img3);
+    //     $("#hackSeaDesc3").text(hackSeaInfo.description3);
+    //
+    // } else if ($(this).attr("id") == "hacc4EduContainer"){
+    //   $("#modalContentGrid").empty();
+    //   generateHTML("hacc")
+    //   // $("#projImg").css("background-image", haccInfo.img);
+    //   // $("#projDesc").text(haccInfo.description);
+    //   // $("#visitSiteButton > a").attr("href", haccInfo.site);
+    //   // $("#visitGithubButton > a").attr("href", haccInfo.gitHub);
+    // } else if ($(this).attr("id") == "bluePlanetContainer"){
+    //   $("#modalContentGrid").empty();
+    //   // $("#projImg").css("background-image", bluePlanetInfo.img);
+    //   // $("#projDesc").text(bluePlanetInfo.description);
+    //   // $("#visitSiteButton > a").attr("href", bluePlanetInfo.site);
+    //   // $("#visitGithubButton > a").attr("href", bluePlanetInfo.gitHub);
+    // } else if ($(this).attr("id") == "marvelApiContainer"){
+    //   $("#modalContentGrid").empty();
+    //   // $("#projImg").css("background-image", marvelApiInfo.img);
+    //   // $("#projDesc").text(marvelApiInfo.description);
+    //   // $("#visitSiteButton > a").attr("href", marvelApiInfo.site);
+    //   // $("#visitGithubButton > a").attr("href", marvelApiInfo.gitHub);
+    // } else if ($(this).attr("id") == "sushiWebContainer"){
+    //   $("#modalContentGrid").empty();
+    //   // $("#projImg").css("background-image", sushiInfo.img);
+    //   // $("#projDesc").text(sushiInfo.description);
+    //   // $("#visitSiteButton > a").attr("href", sushiInfo.site);
+    //   // $("#visitGithubButton > a").attr("href", sushiInfo.gitHub);
+    // } else if ($(this).attr("id") == "constructionWebContainer"){
+    //   $("#modalContentGrid").empty();
+    //   // $("#projImg").css("background-image", constructionInfo.img);
+    //   // $("#projDesc").text(constructionInfo.description);
+    //   // $("#visitSiteButton > a").attr("href", constructionInfo.site);
+    //   // $("#visitGithubButton > a").attr("href", constructionInfo.gitHub);
+    // }
 });
 
 
