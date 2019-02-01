19295ed75137a0b39d63ff34cb260167c7e5609e
Kyle <kyle@grace.local>
2019-02-01 08:44:39 -1000

    adding about me

--- src/About Me/About.js
+++ src/About Me/About.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
-import './About.css'
+import React, { Component } from "react";
+import "./About.css";
 
 export default class About extends Component {
-    render() {
-        return (
-            <div className="aboutMeContainer">test</div>
-        )
-    }
+  render() {
+    return (
+      <div className="aboutMeContainer">
+        <h1 className="aboutMeHeader">About Me</h1>
+        <div className="aboutMeGrid">
+          <div className="aboutMePicWrap">
+            <div className="aboutMePic" />
+          </div>
+          <div className="aboutMeBio">
+            <p>Lorem</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
